6153cde8c9845f049719762b90090a231545fad5
Godsgift ThankGod Akari <giftakari@yahoo.com>
2019-11-07 19:06:58 +1100

    Add button setup

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const courses = document.querySelector("#courses-list");
+const courses = document.querySelector("#courses-list"),
+  shoppingCartContent = document.querySelector("#cart-content tbody");
 
 //Events
 
@@ -6,13 +7,23 @@ loadEventListeners();
 
 function loadEventListeners() {
   courses.addEventListener("click", buyCourses);
+
+  //Remove from cart event
+  shoppingCartContent.addEventListener("click", removeCourse);
 }
 
 function buyCourses(e) {
+  e.preventDefault();
   //Event delegation
 
   if (e.target.classList.contains("add-to-cart")) {
-    e.preventDefault();
-    console.log("Added");
+    const course = e.target.parentElement.parentElement;
+
+    //Read the values
+    getCourseInfo(course);
   }
+}
+
+function getCourseInfo(course) {
+  //Create an object with course data
 }
